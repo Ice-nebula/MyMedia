34a7e2de0c15bfae5f713108607b3842f65fd241
Ice-nebula <soponhantae@gmail.comssh-keygen>
2022-05-20 10:50:45 +0700

    add files app to project to handle file
    add date_created and description props to playlists

--- libraries/models.py
+++ libraries/models.py
@@ -8,7 +8,7 @@ class Library(models.Model):
 	name = models.CharField(blank=False,null=False,max_length=200,db_index=True)
 	description = models.TextField(blank=False,null=False)
 	date_created = models.DateTimeField(auto_now=True,null=False,editable=False)
-	auther = models.ForeignKey(User,on_delete=models.CASCADE)
+	auther = models.ForeignKey(User,on_delete=models.CASCADE,related_name="auther")
 	def __str__(self) -> str:
 		return self.name
 	def get_absolute_url(self):
--- playlists/models.py
+++ playlists/models.py
@@ -5,6 +5,8 @@ from libraries.models import Library
 class playlist(models.Model):
 	id = models.UUIDField(primary_key=True,default=uuid.uuid4,null=False,editable=False)
 	name = models.CharField(blank=False,null=False,max_length=200)
+	description = models.TextField(null=True,blank=True)
+	date_created = models.DateTimeField(auto_now=True,null=False,editable=False)
 	library = models.ForeignKey(Library,related_name="playlist",on_delete=models.CASCADE)
 	def __str__(self)->str:
 		return self.name
